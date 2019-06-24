f406b7eefbdd74518de8673984df4dd884fae6a5
wanghao745 <15271835932@163.com>
2019-06-24 23:39:11 +0800

    babel配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
-module.exports  = {
+module.exports = {
     mode: 'development',      // 模式，表示dev环境
     entry: './src/index.js',  // 入口文件
-    module: {},               // 让 webpack 能够去处理那些非 JavaScript 文件
+    module: {                 // 让 webpack 能够去处理那些非 JavaScript 文件
+        rules: [{
+            test: /\.js$/,    // 注意这里要写正确，不然useBuiltIns不起作用
+            exclude: /node_modules/, // 排除node_modules中的代码
+            use: [{
+                loader: 'babel-loader', // 只是babel和webpack之间的桥梁，并不会将代码转译
+            }],
+        }]
+    },               
     plugins: [],              // 插件
     output: {
         filename: 'bundle.js',  // 打包后文件名称
--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+// import './index.less';
+
+class App extends Component {
+  componentDidMount() {
+    this.getInfo();
+  }
+	getInfo = () => '魑魅魍魉'
+	render() {
+	  return (
+			<div className="content">
+                ddd
+			</div>
+	  );
+	}
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
